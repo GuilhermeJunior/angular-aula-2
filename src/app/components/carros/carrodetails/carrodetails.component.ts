5b0e304fdf639a3eb59183efcc11d3abfdc4b000
Guilherme Chamorro <poltegaisnt@gmail.com>
2025-09-02 17:15:51 -0300

    Adicionar parte 2 (ref parte 3 da aula do professor Wellington)

--- src/app/components/carros/carrodetails/carrodetails.component.ts
+++ src/app/components/carros/carrodetails/carrodetails.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Carro } from '../../../models/carro';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-carrodetails',
@@ -11,9 +13,39 @@ import { Carro } from '../../../models/carro';
 export class CarrodetailsComponent {
 
   carro: Carro = new Carro(0, "");
+  router = inject(ActivatedRoute);
+  router2 = inject(Router);
+
+  constructor() {
+    let id = this.router.snapshot.params['id'];
+    if (id > 0) {
+      this.findById(id)
+    }
+  }
+
+  findById(id: number) {
+    // busca no backend mockada
+    let carroRetornado: Carro = new Carro(id, "Fiesta");
+    this.carro = carroRetornado;
+  }
 
   salvar() {
-    console.log("salvar")
+    if (this.carro.id > 0) {
+    Swal.fire({
+      title: "Editado com Sucesso!",
+      icon: "success",
+      confirmButtonText: 'Ok'
+    });
+    this.router2.navigate(['/admin/carros'], { state: { carroEditado: this.carro } });
+
+    } else {
+       Swal.fire({
+        title: "Salvo com Sucesso!",
+        icon: "success",
+        confirmButtonText: 'Ok'
+       });
+    this.router2.navigate(['/admin/carros'], { state: { carroNovo: this.carro } });
+    }
   }
 
 }
--- src/app/components/carros/carrolist/carrolist.component.ts
+++ src/app/components/carros/carrolist/carrolist.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Carro } from '../../../models/carro';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carrolist',
@@ -12,11 +13,48 @@ export class CarrolistComponent {
    lista: Carro[] = [];
 
    constructor() {
+    let carroNovo = history.state.carroNovo;
+    let carroEditado = history.state.carroEditado;
+
+
      this.lista.push(new Carro(1, "Siena"));
      this.lista.push(new Carro(2, "Gol"));
+     this.lista.push(new Carro(3, "Civic"));
+     this.lista.push(new Carro(4, "Sandero"));
+
+    if (carroNovo) {
+      carroNovo.id = 3939;
+      this.lista.push(carroNovo);
+    }
+
+     if (carroEditado) {
+     let indice = this.lista.findIndex(x => {
+        return x.id == carroEditado.id;
+      } );
+      this.lista[indice] = carroEditado;
+     }
    }
 
-   deletar() {
+   deleteById(carro: Carro) {
+    Swal.fire({
+          title: "Tem certeza que deseja deletar o registro",
+          icon: "warning",
+          showConfirmButton: true,
+          showDenyButton: true,
+          confirmButtonText: 'Sim',
+          denyButtonText: 'Não'
+        }).then((result) => {
+          if (result.isConfirmed) {
+              let indice = this.lista.findIndex(x => { return x.id == carro.id;});
+              this.lista.splice(indice, 1);
+              Swal.fire({
+                title: "Deletado com Sucesso!",
+                icon: "success",
+                confirmButtonText: 'Ok'
+               });
+          }
+        });
+
 
    }
 }
